d3d3cd446eb9d2de1287bcd05e2e0f20dfa312bb
Skie <ezdecode@gmail.com>
2025-12-15 11:37:03 +0530

    2620. Counter

---
+++ counter.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    
+    return function() {
+        return n++;
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
+
+const counter = createCounter(5);
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
+
+
+/*
+Lexical scope -> Where variables are written
+Closure -> Function remembers those variables 
+*/
